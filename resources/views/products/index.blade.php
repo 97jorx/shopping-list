04b7734fa45720d258a7421995f6c3a568c33e7e
97jorx <97jorx@gmail.com>
2022-09-19 21:32:15 +0200

    [ADD] Cambio simple de diseño
    Muevo de sitio iconos de Fontawesome y añado una apariencia distinta en products/index

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -4,19 +4,20 @@
 
 @section('content')
 
+    <div class="product-buttons mb-5">
+        <button class="border-2 cursor-pointer rounded-2xl hover:border-lime-500 border-gray-400 font-bold py-4 px-4 rounded inline-flex items-center">
+            <i class="text-grey-300 fa fa-plus-circle" aria-hidden="true"></i>
+            <span class='pl-3'>Añadir producto</span>
+        </button>
+    </div>
+
     @if ($products->isNotEmpty())
         <div class="products-box grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-5 ">
             @foreach ($products as $product)
 
-                <div class='flex flex-col text-center rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700'>
-
+                <div class='text-grey-300 font-bold relative text-center shadow-xl border-2 rounded-2xl'>
                     <h2 class="h-12 m-6">{{$product->nombre}}</h2>
-
-                    <div class="text-xl border rounded">
-                        <i class="hover:text-lime-500 text-blue-300 fa fa-plus-circle" aria-hidden="true"></i>
-                        <i class="hover:text-red-500 text-blue-300 fa fa-times-circle" aria-hidden="true"></i>
-                    </div>
-
+                    <i class="cursor-pointer absolute top-0 right-0 text-red-500 text-xl hover:text-2xl mr-1 fa fa-times-circle" aria-hidden="true"></i>
                 </div>
 
             @endforeach
