b8674ade96edff5cce8aed1e078300e5177d8697
97jorx <97jorx@gmail.com>
2022-09-19 22:06:20 +0200

    [MOD] Cambios en apariencia en dispositivos móviles

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -4,20 +4,22 @@
 
 @section('content')
 
-    <div class="product-buttons mb-5">
+    <div class="product-buttons mb-5 mx-6 flex justify-center sm:justify-center md:justify-center lg:justify-start">
         <button class="border-2 cursor-pointer rounded-2xl hover:border-lime-500 border-gray-400 font-bold py-4 px-4 rounded inline-flex items-center">
-            <i class="text-grey-300 fa fa-plus-circle" aria-hidden="true"></i>
+            <i class="text-grey-300  fa fa-plus-circle" aria-hidden="true"></i>
             <span class='pl-3'>Añadir producto</span>
         </button>
     </div>
 
     @if ($products->isNotEmpty())
-        <div class="products-box grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-5 ">
+        <div class="products-box grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-5 md:gap-3 ">
             @foreach ($products as $product)
 
-                <div class='text-grey-300 font-bold relative text-center shadow-xl border-2 rounded-2xl'>
-                    <h2 class="h-12 m-6">{{$product->nombre}}</h2>
-                    <i class="cursor-pointer absolute top-0 right-0 text-red-500 text-xl hover:text-2xl mr-1 fa fa-times-circle" aria-hidden="true"></i>
+                <div class='text-grey-300 lg:w-full md:w-full sm:w-full mx-6 font-bold relative text-center shadow-xl border-2 rounded-2xl'>
+                    <h2 class="h-12 m-4 lg:m-7 md:m-3 sm:m-6">{{$product->nombre}}</h2>
+                    <div class="shadow-lg absolute top-0 right-0 bg-zinc-500">
+                        <i class="cursor-pointer absolute top-0 right-0 text-red-500 text-xl hover:text-2xl mr-1 fa fa-times-circle" aria-hidden="true"></i>
+                    </div>
                 </div>
 
             @endforeach
