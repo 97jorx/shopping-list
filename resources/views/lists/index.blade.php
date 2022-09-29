118303d6af078afb20da1026ce94b689f60bcb94
97jorx <97jorx@gmail.com>
2022-09-29 17:25:26 +0200

    [ADD] Listas con checkbox personalizado

--- resources/views/lists/index.blade.php
+++ resources/views/lists/index.blade.php
@@ -12,21 +12,22 @@
     @if ($lists->isNotEmpty())
         <div class="flex justify-center">
             <div class="mt-10 border-2 w-132 h-full rounded-2xl border-zinc-600 ">
-                @foreach ($lists as $list)
-                    <div class="border-b-2 border-gray-500">
-                        <div class="m-8 pl-5">
-                            {{-- <i class="fa-regular fa-circle" aria-hidden="true"></i> --}}
-                            <input id="check_1" type="checkbox"/>
-                            <span class="pl-8">{{$list->nombre}}</span>
+                @foreach ($lists as $key => $list)
+                    <label for='checkbox{{$key}}'>
+                        <div class="border-b-2 border-gray-500 cursor">
+                            <div class="flex items-center  h-24 pl-5">
+                                <input id='checkbox{{$key}}' type="checkbox" class="h-4 w-4 rounded-full shadow" />
+                                <span class="pl-8">{{$list->nombre}}</span>
+                            </div>
                         </div>
-                    </div>
-                    @endforeach
-                    <div id="footer-list" class="flex justify-center mt-5 mb-5 space-x-10 text-xs uppercase">
-                       <div class="basis-2/4">Mostrando {{count($lists)}} elementos</div>
-                       <div><a href="">Marcar todas</a></div>
-                       <div><a href="">Eliminar</a></div>
-                    </div>
+                    </label>
+                @endforeach
+                <div id="footer-list" class="flex justify-center mt-5 mb-5 space-x-10 text-xs uppercase">
+                    <div class="basis-2/4">Mostrando {{count($lists)}} elementos</div>
+                    <div><a href="">Marcar todas</a></div>
+                    <div><a href="">Eliminar</a></div>
                 </div>
+            </div>
         </div>
         @else
         <div class="max-w-screen-lg bg-white mx-auto rounded-lg text-center py-11 mt-4">
