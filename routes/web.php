59fb1943f00118ff6e3ecce229fdb6e5bdf8ed28
97jorx <97jorx@gmail.com>
2022-10-13 19:55:18 +0200

    [FIX] Redirect to home

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('lists/index');
+    return redirect('lists/index');
 })->name('home');
 
 Route::controller(ProductController::class)->group(function() {
